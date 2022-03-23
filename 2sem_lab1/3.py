8bb8e86514b04cc933f2cf7a581c739c935dbe64
lopatich <91071908+lopatich@users.noreply.github.com>
2022-03-23 10:53:46 +0300

    Add files via upload

--- 2sem_lab1/3.py
+++ 2sem_lab1/3.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 x = np.arange(-10, 10.01, 0.01)
+sp = plt.subplot(111)
 plt.plot(x, x*x - x - 6)
+plt.scatter(-2, 0)
+plt.scatter(3, 0)
 plt.show()
