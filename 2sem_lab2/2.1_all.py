da41caa457e03362472fdd51c61d76ad9b2fa40f
lopatich <91071908+lopatich@users.noreply.github.com>
2022-03-23 22:43:41 +0300

    Add files via upload

--- 2sem_lab2/2.1_all.py
+++ 2sem_lab2/2.1_all.py
@@ -4,18 +4,28 @@ import re
 
 x = []
 y = []
-sp2 = plt.subplot(111)
+sp1 = plt.subplot(111)
+
 plt.xlabel('Concentration, nM')
 plt.ylabel('Response')
 plt.grid(True, axis='y')
 
-
+with open('wild_type.txt', 'r') as wild:
+    for line in wild:
+        string = wild.readline()
+        str1 = string.replace('\n', '\t')
+        data = re.split("\t", str1)
+        sp1.scatter(data[0], data[1])
 
 with open('mutant.txt', 'r') as mutant:
     for line in mutant:
         string = mutant.readline()
         str1 = string.replace('\n', '\t')
         data = re.split("\t", str1)
-        sp2.scatter(data[0], data[1])
+        sp1.scatter(data[0], data[1])
 
 plt.show()
+
+
+
+
